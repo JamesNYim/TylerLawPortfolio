c2610cf1782a4b0b96f359a646ef373fdce99632
JamesNYim <101849565+JamesNYim@users.noreply.github.com>
2025-10-13 18:51:33 -0700

    Contact page finished

--- frontend/src/components/ContactForm.jsx
+++ frontend/src/components/ContactForm.jsx
@@ -31,17 +31,91 @@ export default function ContactForm() {
 
     
     return (
-        <form ref={form} onSubmit={sendEmail}>
-            <label>Name</label>
-            <input type="text" name="user_name" />
+        <div className="ContactForm" style={{
+            marginTop: '25vh',
+            height: '100vh',
+        }}>
+            <style> 
+            {`
+                input::placeholder,
+                textarea::placeholder {
+                    color: grey;
+                    font-weight: lighter;
+                    font-family: Playfair Display;
+                },
+            `}
+            </style>
+            <h1 style={{fontFamily: 'Playfair Display', fontWeight: 'lighter', color: 'grey', textAlign: 'center'}}>Contact</h1>
+            <form ref={form} onSubmit={sendEmail} style={{
+                display: 'flex',
+                justifyContent: 'center',
+                flexDirection: 'column',
+            }}>
+                
+                <div className="Name" style ={formSectionStyle}>
+                    <input required type="text" name="user_name" placeholder="name" style={{
+                        ...textareaStyle, 
+                        height: '2em',
+                        borderBottom: '0px'
+                    }}/>
+                </div>
 
-            <label>Email</label>
-            <input type="email" name="user_email" />
-        
-            <label>Message</label>
-            <textarea name="message" />
+                <div className="Email" style ={formSectionStyle}>
+                    <input required type="email" name="user_email" placeholder="email" style={{
+                        ...textareaStyle, 
+                        height: '2em',
+                        borderBottom: '0px'
+                    }}/>
+                </div>
+            
+                <div className="Message" style ={formSectionStyle}>
+                    <textarea required name="message" placeholder="message" style={{
+                        ...textareaStyle, 
+                        height: '16em'
+                    }}/>
+                </div>
 
-            <input type="submit" value="Send" />
-        </form>
+                <input 
+                    type="submit" 
+                    value="Send Message" 
+                    style ={{
+                        fontSize: 'inherit',
+                        fontFamily: 'inherit',
+                        fontWeight: 'lighter',
+                        width: '25em',
+                        height: '3em',
+                        margin: '24px auto',
+                        cursor: 'pointer',
+                        backgroundColor: 'transparent',
+                        border: '2px solid grey',
+                        color: 'grey',
+                    }}
+                    onMouseOver= {(e) => {
+                        e.target.style.fontStyle='italic';
+                    }}
+                    onMouseOut= {(e) => {
+                        e.target.style.fontStyle='normal';
+                    }}
+                />
+            </form>
+        </div>
     );
 }
+
+const formSectionStyle = { 
+    width: '50%', 
+    margin: '0 auto', 
+    display: 'flex', 
+    flexDirection: 'column', 
+    justifyContent: 'center',
+    fontFamily: 'Playfair Display',
+    fontSize: '24px',
+};
+
+const textareaStyle = {                         
+    borderRadius: '0px',
+    border: "2px solid #ccc",
+    fontFamily: 'inherit',
+    fontSize: 'inherit',
+    color: 'grey',
+};
